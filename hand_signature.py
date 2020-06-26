eadf5235c1d7783a361efe6663a59e94b9a056f5
ronstumuhairwe <ronstumuhairwe@gmail.com>
2020-06-26 09:03:21 +0300

    Added more layers to our neural network and a dropout to reduce overfitting

--- hand_signature.py
+++ hand_signature.py
@@ -42,7 +42,9 @@ display_sample(1234)
 
 model = Sequential()
 model.add(Dense(512,activation='relu',input_shape=(784,)))
-model.add(Dense(512,activation='relu',input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512,activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(10,activation='softmax'))
 model.summary() 
 
